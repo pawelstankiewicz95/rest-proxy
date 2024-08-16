8438bbe4c007f2f955c284972ebec946a1d285b2
pawelstankiewicz <pawelsta95@gmail.com>
2024-08-16 23:45:10 +0200

    init commit

---
+++ src/test/java/com/pawelapps/restproxy/RestProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawelapps.restproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
